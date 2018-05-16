cedcf2688261dc73de029b538ee289100ecc26e1
707 <707472783@qq.com>
2018-05-17 00:31:53 +0800

    更新日志

--- Source/debugging/logger.cpp
+++ Source/debugging/logger.cpp
@@ -1,4 +1,10 @@
 #include "logger.h"
+#include "../third/json.hpp"
+
+#include <fstream>
+
+using Json = nlohmann::json;
+
 /**
  * 日志初始化 
  */
@@ -6,6 +12,14 @@ void Logger::init(const std::string logFile) {
     // t:)表示时间
     // l:)表示错误等级
     // m:)表示信息
+    std::ifstream iLogConf(logFile);
+    Json jLogConf;
+    if (!iLogConf) {
+        // 载入日志配置文件失败
+    }
+    iLogConf >> jLogConf;
+    m_format = jLogConf["format"];
+    m_path = jLogConf["file"]["path"];
 }
 
 void Logger::destroy() {
--- Source/debugging/logger.h
+++ Source/debugging/logger.h
@@ -3,9 +3,20 @@
 
 class Logger
 {
+private:
+    // 日志格式
+    std::string m_format;
+    // 日志输出文件路径，有路径则输出在文件，没有则不输出
+    std::string m_path;
+    // 日志是否输出在终端
+    bool m_console;
+    
+
 public:
     // 初始化日志
     static void init(const std::string logFile);
     // 销毁日志
     static void destroy();
+    // 打印日志
+    static void log();
 };
