12d0bcd9b95bc0179e22b3ef6361df4550024d31
707 <707472783@qq.com>
2018-05-18 00:20:37 +0800

    更新logger

--- Source/debugging/logger.cpp
+++ Source/debugging/logger.cpp
@@ -5,21 +5,24 @@
 
 using Json = nlohmann::json;
 
+Logger* Logger::logger = nullptr;
 /**
  * 日志初始化 
  */
-void Logger::init(const std::string logFile) {
-    // t:)表示时间
-    // l:)表示错误等级
-    // m:)表示信息
-    std::ifstream iLogConf(logFile);
-    Json jLogConf;
-    if (!iLogConf) {
-        // 载入日志配置文件失败
+Logger* Logger::getLogger() {
+    if (logger == nullptr) {
+        // t:)表示时间
+        // l:)表示错误等级
+        // m:)表示信息
+        std::ifstream iLogConf(m_path);
+        Json jLogConf;
+        if (!iLogConf) {
+            // 载入日志配置文件失败
+        }
+        iLogConf >> jLogConf;
+        this->m_format = jLogConf["format"];
+        m_path = jLogConf["file"]["path"];
     }
-    iLogConf >> jLogConf;
-    m_format = jLogConf["format"];
-    m_path = jLogConf["file"]["path"];
 }
 
 void Logger::destroy() {
--- Source/debugging/logger.h
+++ Source/debugging/logger.h
@@ -7,16 +7,18 @@ private:
     // 日志格式
     std::string m_format;
     // 日志输出文件路径，有路径则输出在文件，没有则不输出
-    std::string m_path;
+    static std::string m_path = "logging.json";
     // 日志是否输出在终端
     bool m_console;
-    
+    // 日志单例模式
+    static Logger *logger;
 
 public:
-    // 初始化日志
-    static void init(const std::string logFile);
-    // 销毁日志
-    static void destroy();
     // 打印日志
-    static void log();
+    void log();
+    static Logger* getLogger();
+
+protected:
+    // 声明为protected是为了封锁创建实例的入口
+    Logger();
 };
